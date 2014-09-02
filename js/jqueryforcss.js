290c1038beead579feb07b81762b2f65939b0659
youanan <youanan@163.com>
2014-09-02 21:26:31 +0800

    add friends

--- js/jqueryforcss.js
+++ js/jqueryforcss.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
  $("table").wrap('<div class="table-responsive"/>');
  $("table").addClass("table table-bordered table-striped table-hover");
+ $("a[href^='http*://']").each(function(){
+ this.target="_blank";
+ });
  });
 
