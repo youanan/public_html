fe010a0e7f24d30bec91a14d84a41ff961f17634
youanan <youanan@163.com>
2015-02-17 10:53:18 +0800

    更新

---
+++ js/tttime/init.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function() {
+						   
+	$("#countdown").countdown({
+				date: "30 August 2009 11:50:00",
+				format: "on"
+			},
+			
+			function() {
+				// callback function
+			});
+
+});	
---
+++ js/xxtime/init.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function() {
+						   
+	$("#countdown").countdown({
+				date: "28 January 2015 11:11:00",
+				format: "on"
+			},
+			
+			function() {
+				// callback function
+			});
+
+});	
