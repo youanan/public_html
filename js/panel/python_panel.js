4d3d95e0eda0d019514e1761650acb85f2b20ac0
youanan <youanan@163.com>
2015-03-03 21:23:26 +0800

    python ex26-39

--- js/panel/python_panel.js
+++ js/panel/python_panel.js
@@ -13,7 +13,13 @@ $(document).ready(function(){
  $("p:contains(~~~~)").each(function(){
  $(this).replaceWith('<div class="panel panel-danger">'+'<div class="panel-heading">Warning</div>'+'<div class="panel-body">'+$(this).html()+'</div>'+'</div>');
  });
+
  $("p:contains(~~~)").each(function(){
  $(this).replaceWith('<div class="panel panel-success">'+'<div class="panel-heading">Note</div>'+'<div class="panel-body">'+$(this).html()+'</div>'+'</div>');
  });
+
+ $(".toc").addClass("well page-nav");
+ $("table").wrap('<div class="table-responsive"/>');
+ $("table").addClass("table table-bordered table-striped table-hover");
+
 });
